314e66a1dac2f695460e29088afe6aa17512a589
Vipul Rathod <VRathod@magnitude.com>
2022-12-05 14:35:37 +0530

    Marketing partner addition

--- ticket/views.py
+++ ticket/views.py
@@ -19,19 +19,14 @@ def __generateTicket(inTicket: Ticket) -> HttpResponse:
     templatePath = os.path.join(settings.BASE_DIR, 'static/front.png')
     factory = TicketFactory(inTicket.name, inTicket.contact, inTicket.seats, inTicket.total, inTicket.refId, templatePath)
     factory.FontPath = 'static/Segoe-UI-Font/SEGOEUI.TTF'
-    # factory.AddrSeats = (1280, 300)
-    # factory.AddrSeatCount = (1475, 300)
-    # factory.AddrTicketID = (1325, 350)
-    # factory.AddrTicketQRCode = (1275, 105)
-    # factory.AddrBuyerName = (110, 250)
     factory.AddrSeats = (1600, 385)
     factory.AddrSeatCount = (1850, 380)
     factory.AddrTicketID = (1675, 445)
     factory.AddrTicketQRCode = (1625, 150)
     factory.AddrBuyerName = (110, 325)
     buff = io.BytesIO()
-    factory.generate().save(buff, 'jpeg')
-    response = HttpResponse(buff.getvalue(), content_type='image/jpeg')
+    factory.generate().save(buff, 'png')
+    response = HttpResponse(buff.getvalue(), content_type='image/png')
     return response
 
 
